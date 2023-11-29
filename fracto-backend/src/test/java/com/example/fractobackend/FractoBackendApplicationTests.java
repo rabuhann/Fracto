1e4c389edb4e6bee995e6b800eeae5590327a167
akash1521439 <39040925+akash1521439@users.noreply.github.com>
2023-11-29 15:32:45 -0600

    Initialize project

---
+++ fracto-backend/src/test/java/com/example/fractobackend/FractoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fractobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
