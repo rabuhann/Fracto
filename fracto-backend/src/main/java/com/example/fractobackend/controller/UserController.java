44674c2471e7329865b504a9998f6fd42b589732
rabuhann <92608181+rabuhann@users.noreply.github.com>
2023-12-07 00:41:03 +0400

    Beginning CRUD

--- fracto-backend/src/main/java/com/example/fractobackend/controller/UserController.java
+++ fracto-backend/src/main/java/com/example/fractobackend/controller/UserController.java
@@ -1,29 +1,67 @@
 package com.example.fractobackend.controller;
 
+import com.example.fractobackend.exception.ResourceNotFoundException;
+import com.example.fractobackend.entity.User;
+import com.example.fractobackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.example.fractobackend.service.UserService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
-//@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/")
 public class UserController {
-//	@Autowired
-//	private UserService userService;
-//	
-//	@GetMapping("/login")
-//    public String loginForm(){
-//        return "login";
-//    }
-//	
-//	@GetMapping("/register")
-//    public String registerForm(){
-//        return "register";
-//    }
-	
-	
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Get all users
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Create user Rest API
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Get user by ID Rest API
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User doesn't exists with id: " + id));
+        return ResponseEntity.ok(user);
+    }
+
+    // Update user REST API
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User doesn't exists with id: " + id));
+
+        user.setUserEmail((userDetails.getUserEmail()));
+        user.setUserName(userDetails.getUserName());
+        user.setPassword(userDetails.getPassword());
+
+        User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // Delete user REST API
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User doesn't exists with id: " + id));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 }
