8e2143b37448989f9ee5812bc68e513e50acdf5d
rabuhann <92608181+rabuhann@users.noreply.github.com>
2023-12-07 00:47:48 +0400

    Fix revert conflicts

--- fracto-frontend/src/app/login/login.component.ts
+++ fracto-frontend/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserAuthService } from '../_services/user-auth.service';
 import { UserService } from '../_services/user.service';
 
 @Component({
@@ -12,24 +11,15 @@ import { UserService } from '../_services/user.service';
 export class LoginComponent implements OnInit {
   constructor(
     private userService: UserService,
-    private userAuthService: UserAuthService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   login(loginForm: NgForm) {
     this.userService.login(loginForm.value).subscribe(
       (response: any) => {
-        this.userAuthService.setRoles(response.user.roles);
-        const role = response.user.roles[0].name;
-        if (role === 'ROLE_ADMIN') {
-          this.router.navigate(['/admin']);
-          this.userAuthService.setAuthenticated(true);
-        } else {
-          this.router.navigate(['/user']);
-          this.userAuthService.setAuthenticated(true);
-        }
+        console.log(response)
       },
       (error) => {
         console.log(error);
