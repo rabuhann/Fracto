7e58400f8660407ac4d04c3aab451935a5807ac5
rabuhann <92608181+rabuhann@users.noreply.github.com>
2023-12-07 18:49:22 +0400

    Fix backend problem

---
+++ fracto-frontend/src/app/user-list/user-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService,
+    private router: Router) {
+    this.users = [];
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.userService.getUsersList().subscribe((data: User[]) => {
+      console.log(data);  // Log data to console
+      this.users = data;
+    });
+  }
+
+  userDetails(id: number) {
+    this.router.navigate(['user-details', id]);
+  }
+
+  updateUser(id: number) {
+    this.router.navigate(['update-user', id]);
+  }
+
+  deleteUser(id: number){
+    this.userService.deleteUser(id).subscribe( (data: any) => {
+      console.log(data);
+      this.getUsers();
+    })
+  }
+}
