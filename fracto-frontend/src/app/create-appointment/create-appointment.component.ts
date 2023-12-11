ef3d368d45cbc0c03234f23e62d3f9fba851a918
rabuhann <92608181+rabuhann@users.noreply.github.com>
2023-12-11 23:13:10 +0400

    Initialized Book an Appointment page with Specialization -> Doctor -> TimeSlot workflow

--- fracto-frontend/src/app/create-appointment/create-appointment.component.ts
+++ fracto-frontend/src/app/create-appointment/create-appointment.component.ts
@@ -1,12 +1,12 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-@Component({
-  selector: 'app-create-appointment',
-  templateUrl: './create-appointment.component.html',
-  styleUrls: ['./create-appointment.component.css']
-})
-export class CreateAppointmentComponent implements OnInit, AfterViewInit {
 
+@Component({
+   selector: 'app-create-appointment',
+   templateUrl: './create-appointment.component.html',
+   styleUrls: ['./create-appointment.component.css']
+})
+export class CreateAppointmentComponent implements OnInit {
 
   @ViewChild('myclient') client!: ElementRef | undefined;
 
@@ -37,40 +37,67 @@ export class CreateAppointmentComponent implements OnInit, AfterViewInit {
   }
 
   specializations: string = '';
-  doctorId: number = 0;
-  doctorName: string = '';
-  specialization: string = '';
-  timeslotId: number = 0;
-  availableDateTime: string = '';
+  selectedSpecialization: string = '';
 
-  buildingId: string = '';
-  floorId: string = ''; clients: any[] = []; sites: any[] = [];
-  doctors: any[] = []; floors: any[] = [];
+  doctors: any[] = [];
+  selectedDoctor: any;
 
-  ngOnInit() {
-    this.loadSpecializations();
+  selectedTimeslot!: any; // Adjust the type accordingly based on the structure of your timeslot object
 
-  }
+   timeslots: any[] = [];
+
+   ngOnInit() {
+      this.loadSpecializations();
+   }
+
   ngAfterViewInit() {
     if (this.client != null) {
     }
   }
 
   loadSpecializations() {
-    this.http.get('localhost:8080/api/v1/specializations').subscribe((result: any) => {
+    this.http.get('http://localhost:8080/api/v1/specializations').subscribe((result: any) => {
       this.specializations = result;
-    })
+    });
   }
-  
-  // getDoctorsBySpecialization() {
-  //   this.http.get('localhost:8080/api/v1/specialization').subscribe((result: any) => {
-  //     this.doctorId = result.data;
-  //   })
-  // }
-  // getTimeslotByDoctor() {
-  //   this.http.get('localhost:8080/api/v1/time-slots/doctor/' + this.doctorId).subscribe((result: any) => {
-  //     this.doctors = result.data;
-  //   })
-  // }
 
+  getDoctorsBySpecialization() {
+    this.http.get<any[]>('http://localhost:8080/api/v1/specialization/' + this.selectedSpecialization)
+      .subscribe((result: any[]) => {
+        this.doctors = result;
+        console.log('Doctors:', this.doctors);
+  
+        // Check if there is at least one doctor
+        if (this.doctors.length > 0) {
+          // Set the selectedDoctor to the first doctor in the list
+          this.selectedDoctor = this.doctors[0];
+          console.log('Selected Doctor:', this.selectedDoctor);
+        } else {
+          // Clear selectedDoctor if there are no doctors
+          this.selectedDoctor = undefined;
+        }
+      });
+  }   
+
+  getTimeslotByDoctor() {
+    if (this.selectedDoctor) {
+      const selectedDoctorId = this.selectedDoctor.doctorId;
+      this.http.get<any[]>('http://localhost:8080/api/v1/time-slots/doctor/' + selectedDoctorId)
+        .subscribe((result: any[]) => {
+          this.timeslots = result; // Assuming you have a timeslots array
+        });
+    }
+  }  
+
+ saveAppointment() {
+    if (this.selectedTimeslot) {
+       // Here you can use this.selectedTimeslot.availableDateTime or any other properties
+       console.log('Selected Timeslot:', this.selectedTimeslot);
+    }
+    // Implement your save logic here
+ }
+
+ refreshData() {
+    // Implement your refresh logic here
+ }
 }
